71a5bbd2d068536ff48d4970c1dabb686fcb393a
ifamille <jianguo.annonce@gmail.com>
2014-09-05 16:50:12 +0200

    update

--- pinyinStandard2Variant.py
+++ pinyinStandard2Variant.py
@@ -1,6 +1,14 @@
+# coding=utf-8
 import sys
-
+# read file
+chinesePhoneWithTone = {'ā':'a1','á':'a2'}
 f = open(sys.argv[1],'r')
 wordList = list(f)
+# read lines and execute treatment one by one
 for word in wordList:
-    print(word),
+    # extract each syllable of one word
+    syllables = word.split()
+    for syllable in syllables:
+        print(syllable)
+        phone = list(syllable)
+    print(chinesePhoneWithTone['á']+"\n"),
