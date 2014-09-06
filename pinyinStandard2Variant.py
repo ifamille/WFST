e5d441a2ff385096bd36f3bfb3902166be96412b
ifamille <jianguo.annonce@gmail.com>
2014-09-06 02:07:02 +0200

    clean up

--- pinyinStandard2Variant.py
+++ pinyinStandard2Variant.py
@@ -15,27 +15,26 @@ wordList = list(f)
 for word in wordList:
     # extract syllables of one word
     syllables = word.split()
-    # iterate each syllable of one word
+    # iterate each syllable of one word, for example, TIAN or AN or MEN
     for syllable in syllables:
+        # create list for conserve phones, for example,[t,i,a,n]
         phones = list(unicode(syllable,'utf-8'))
         numPhones = len(phones)
+        # justify each phone is wheather tone or not
         for i in range(numPhones):
-           if phones[i] > u'z':
-               #tone_set.append(phone)
-               #print('_' + tone_dic[phone] + '_'),
-               tone = tone_dic[phones[i]]
-               phones[i] = tone[0]
-               phones.append(tone[1])
-          # else:
-          #     print(phone),
-        print(phones)
-        print('\n')
+            # when character is found in tone dictionary
+            if phones[i] > u'z':
+                # map to variant format, for example, a1
+                tone = tone_dic[phones[i]]
+                # conserve phone with tone to phone not having tone
+                phones[i] = tone[0]
+                # conserve tone at the end of list of phone
+                phones.append(tone[1])
+        # iterate each element in the list of phones and print them into a file
         for phone in phones:
             pen.write(phone.encode('utf-8'))
+        # there is a white space between syllables
         pen.write(' ')
-  #  print("\n")
-#for tone in tone_set:
-#    pen.write(tone.encode('utf-8'))
     pen.write('\n')
 f.close()
 pen.close()
