70e8bd83f827e272545944cb5f22184d6f1461ff
ifamille <jianguo.annonce@gmail.com>
2014-09-13 13:13:14 +0200

    add script

--- cleanNewsBackup.sh
+++ cleanNewsBackup.sh
@@ -1,10 +1,7 @@
 #!bin/bash
-rm ./Backup/PYSs/*.pys
 rm ./Backup/PYVs/*.pyv
 rm ./Backup/PYWs/*.pyw
 rm ./Backup/ATs/*.sent
-rm ./Backup/TTs/*.tt
 rm ./Backup/PYWVox/*
 rm ./Backup/PYPs/*.pyp
 rm ./Backup/SEGs/*.seg
-rm ./Backup/OTs/*
